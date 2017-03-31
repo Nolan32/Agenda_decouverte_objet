dcaedd2c75b5d404c07acfa54d555bc7ddad9066
nolan32 <jordy.duces@hotmail.frgit config --global user.email jordy.duces@hotmail.frgit config --global user.email jordy.duces@hotmail.fr>
2017-03-31 16:32:47 +0200

    ajout de l'ajout de plusieurs ligne (boucle for) correction multi ligne ajout de la fonction envoyé vers html

--- js/app.js
+++ js/app.js
@@ -1,2 +1,38 @@
-$(document).ready();
-alert ("bonjour");
+var agenda = [];
+var personne = {};
+$("#ajout").click(function(){
+
+
+	personne = {
+		"prenom": $('#inp').val(),
+		"nom": $('#inpN').val(),
+		"age": $('#inpA').val(),
+	};
+
+
+	agenda.push(personne);	
+
+	$('tbody').html('');
+	
+	for (var i = 0; i < agenda.length; i++) {
+		$('tbody').append("<tr><td>" 
+							+ i +"</td><td>" 
+							+ agenda[i].prenom + "</td><td>"
+							+  agenda[i].nom + "</td><td>" 
+							+  agenda[i].age + "</td></tr>");	
+		//agenda.splice("")
+		$('input').val('');
+		console.log(agenda);
+	};
+
+
+});
+
+
+					// local storage
+
+localstorage
+
+
+var monobjet_json = JSON.stringify(personne);
+sessionStorage.setItem("objet",monobjet_json);
